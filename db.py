04fff136136760769c8909bef0e628bbc3d4ea8f
Safiya <147792763+safiya2610@users.noreply.github.com>
2026-01-04 14:33:07 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,43 +2,48 @@ import sqlite3
 
 DB_NAME = "users.db"
 
-conn = sqlite3.connect(DB_NAME, check_same_thread=False)
-cur = conn.cursor()
-
-
 def get_db():
-    return sqlite3.connect(DB_NAME, check_same_thread=False)
+    db = sqlite3.connect(DB_NAME, check_same_thread=False)
+    db.row_factory = sqlite3.Row
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 def init_db():
-   
+    db = sqlite3.connect(DB_NAME)
+    cur = db.cursor()
+
     cur.execute("""
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT UNIQUE,
-        email TEXT UNIQUE,
-        password TEXT,
-        is_private INTEGER DEFAULT 0
-    )
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE,
+            email TEXT UNIQUE,
+            password TEXT,
+            is_private INTEGER DEFAULT 0
+        )
     """)
 
     cur.execute("""
-    CREATE TABLE IF NOT EXISTS images (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_name TEXT,
-        filename TEXT,
-        metadata TEXT,
-        visibility TEXT DEFAULT 'private',
-        narrative TEXT
-    )
+        CREATE TABLE IF NOT EXISTS images (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_name TEXT,
+            filename TEXT,
+            metadata TEXT,
+            visibility TEXT DEFAULT 'private',
+            narrative TEXT
+        )
     """)
 
     cur.execute("""
-    CREATE TABLE IF NOT EXISTS friends (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        sender TEXT,
-        receiver TEXT,
-        status TEXT DEFAULT 'pending'
-    )
+        CREATE TABLE IF NOT EXISTS friends (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            sender TEXT,
+            receiver TEXT,
+            status TEXT DEFAULT 'pending'
+        )
     """)
 
-    conn.commit()
+    db.commit()
+    db.close()
