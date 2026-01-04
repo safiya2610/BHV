86e7fe141655a818a27672f9a30cc8fd36c53db6
Safiya <147792763+safiya2610@users.noreply.github.com>
2026-01-04 13:34:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 import os
 from dotenv import load_dotenv
 
@@ -6,4 +5,7 @@ load_dotenv()
 
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+if not SECRET_KEY:
+    raise RuntimeError("Missing SECRET_KEY environment variable")
