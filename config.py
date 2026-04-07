6e98b3b29357538a4cd7333019641f73864daf29
Safiya <147792763+safiya2610@users.noreply.github.com>
2026-04-07 06:25:08 +0000

    add default value also

--- config.py
+++ config.py
@@ -3,19 +3,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-if not SECRET_KEY:
-    raise RuntimeError("Missing SECRET_KEY")
+IS_RENDER = os.getenv("RENDER") is not None
 
+SECRET_KEY = os.getenv("SECRET_KEY", "test_secret_key")
 
-GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
-GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID", "")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", "")
+GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI", "http://localhost")
 
-if not GOOGLE_CLIENT_ID or not GOOGLE_CLIENT_SECRET or not GOOGLE_REDIRECT_URI:
-    raise RuntimeError("Missing Google OAuth env vars")
+if IS_RENDER:
+    if not SECRET_KEY:
+        raise RuntimeError("Missing SECRET_KEY")
 
-# JWT Configuration
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-REFRESH_TOKEN_EXPIRE_DAYS = 7
+    if not GOOGLE_CLIENT_ID or not GOOGLE_CLIENT_SECRET or not GOOGLE_REDIRECT_URI:
+        raise RuntimeError("Missing Google OAuth env vars")
+
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7))
 JWT_ALGORITHM = "HS256"
